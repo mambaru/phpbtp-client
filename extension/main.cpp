bb47961a511a5f4e567b22e1e86558c5e754f0a1
Vladimir Migashko <migashko@gmail.com>
2021-04-20 17:45:57 +0300

    [api] <<push

--- extension/main.cpp
+++ extension/main.cpp
@@ -16,33 +16,16 @@ extern "C" {
     {
         // static(!) Php::Extension object that should stay in memory
         // for the entire duration of the process (that's why it's static)
-        static Php::Extension extension("btp-pusher", "1.0");
+        static Php::Extension extension("btp-client", "1.1");
 
         extension.add<btp_configure>("btp_configure", {});
         extension.add<btp_create_meter>("btp_create_meter", {});
         extension.add<btp_release_meter>("btp_release_meter", {});
+        extension.add<btp_create_meter>("btp_create_meter2", {});
+        extension.add<btp_release_meter>("btp_release_meter2", {});
         extension.add<btp_pushout>("btp_pushout", {});
         extension.add<btp_add_time>("btp_add_time", {});
         extension.add<btp_add_time>("btp_add_size", {});
-        
-        
-        // @todo    add your own functions, classes, namespaces to the extension
-        /*
-        extension.add<btp_config_server_pool>("btp_config_server_pool", {});
-        extension.add<btp_script_name_set>("btp_script_name_set", {});        
-   
-        extension.add<btp_config_server_set>("btp_config_server_set", {});
-        extension.add<btp_dump>("btp_dump", {});
-        extension.add<btp_dump_timer>("btp_dump_timer", {});
-        extension.add<btp_project_name_set>("btp_project_name_set", {});
-        extension.add<btp_timer_start>("btp_timer_start", {});
-        extension.add<btp_timer_stop>("btp_timer_stop", {});
-        extension.add<btp_flush>("btp_flush", {});
-        extension.add<btp_timer_count>("btp_timer_count", {});
-        extension.add<btp_timer_count_script>("btp_timer_count_script", {});
-        extension.add<btp_timer_set_operation>("btp_timer_set_operation", {});
-        */
-        
         
         // return the extension
         return extension;
