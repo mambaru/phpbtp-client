3ddd53dea98c4e911e6f6e6d790d7bbb9bc8c632
Vladimir Migashko <migashko@gmail.com>
2021-06-11 11:24:23 +0300

    cmake-ci update autocommit

---
+++ extension/test-once.php
@@ -0,0 +1,13 @@
+<?php
+
+#require __DIR__ . '/init.php';
+
+btp_configure('./phpbtp-client.conf');
+
+$btpMeter = btp_create_meter2(1);
+
+usleep(200);
+
+btp_release_meter2($btpMeter, 'test.php', 'my_service_single_run', 'server134', 'test13434');
+btp_pushout();
+echo 'Done' . PHP_EOL;
