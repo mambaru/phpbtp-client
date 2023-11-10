1ac55fca706f172aad77cd469c280b11448e529c
Vladimir Migashko <migashko@gmail.com, migashko@mail.ru, migashko@wamba.com>
2023-11-11 02:14:13 +0300

    gh-pages init

--- libbtpclient/btpclient/types.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <wrtstat/aggregator/api/types.hpp>
-
-namespace wamba{ namespace btp{
-
-typedef wrtstat::id_t id_t;
-//constexpr id_t bad_id = wrtstat::bad_id;
-
-}}
--- libbtpclient/tests/test_btpclient.cpp
+++
@@ -1,6 +0,0 @@
-#include <fas/testing.hpp>
-
-BEGIN_TEST
-  /*RUN_SUITE(udpclient)*/
-  RUN_SUITE(pushout)
-END_TEST
